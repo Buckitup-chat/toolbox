9c5a40ae8033238ca28648e46fb975a7938263e5
s <salseeg@gmail.com>
2026-01-24 09:11:42 +0200

    initial

---
+++ lib/toolbox.ex
@@ -0,0 +1,18 @@
+defmodule Toolbox do
+  @moduledoc """
+  Documentation for `Toolbox`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Toolbox.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
